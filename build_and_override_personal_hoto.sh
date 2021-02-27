03999553260cc5d0f30d47527afebf4b659e7c7c
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2021-02-27 18:37:05 +0000

    Logitech pointer - map escape to space

--- build_and_override_personal_hoto.sh
+++ build_and_override_personal_hoto.sh
@@ -5,4 +5,4 @@ set -eu
 make
 cp public/json/personal_hoto.json ~/.config/karabiner/assets/complex_modifications
 
-bat ~/.config/karabiner/assets/complex_modifications/personal_hoto.json --pager none
+echo "Done"
